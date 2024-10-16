7c4b82c948a9fc72e877ef85a5ece39c64977662
Quester <quester561@gmail.com>
2024-10-16 20:38:23 +0545

    category slider

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,32 +1,4 @@
-# from django.contrib import admin
-# from .models import Category, Quiz, Question, Choice, QuizSubmission, UserRank
-# from unfold.admin import ModelAdmin as UnfoldModelAdmin
 
-# class CategoryAdmin(UnfoldModelAdmin):
-#     pass
-
-# class QuizAdmin(UnfoldModelAdmin):
-#     pass
-
-# class QuestionAdmin(UnfoldModelAdmin):  
-#     pass
-
-# class ChoiceAdmin(UnfoldModelAdmin):  # or use admin.ModelAdmin if you prefer
-#     list_display = ('question', 'text', 'is_correct')
-
-# class QuizSubmissionAdmin(UnfoldModelAdmin):
-#     pass
-
-# class UserRankAdmin(UnfoldModelAdmin):
-#     pass
-
-# # Register your models here.
-# admin.site.register(Category, CategoryAdmin)
-# admin.site.register(Quiz, QuizAdmin)
-# admin.site.register(Question, QuestionAdmin)
-# admin.site.register(Choice, ChoiceAdmin)
-# admin.site.register(QuizSubmission, QuizSubmissionAdmin)
-# admin.site.register(UserRank, UserRankAdmin)
 
 from django.contrib import admin
 from .models import Category, Quiz, Question, Choice, QuizSubmission, UserRank
